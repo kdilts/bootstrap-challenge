4bfdd5dffd54ec8ffc9510d222d12bc4a01c66a9
Kevin Dilts <kdilts13@gmail.com>
2016-11-05 16:48:24 -0600

    added logo image to nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -44,7 +44,9 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Brand</a>
+						<a class="navbar-brand" href="#">
+							<img src="images/logo.jpg" alt="LOGO"/>
+						</a>
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
